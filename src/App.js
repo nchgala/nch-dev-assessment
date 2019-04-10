04203a5e4df0ee548ac1cf87fd49e3703ea201e2
jcabria <julie.cabria@nchcss.com>
2019-04-10 08:50:44 +0100

    added login and homepage comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Login from "./Components/login/login";
+
+
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+
+
+
+
+
+
+
+
+
+          
+          <Route path="/login/" component={Login} />
+          
+        </div>
+      </Router>
     );
-  }
+  }    
 }
 
 export default App;
