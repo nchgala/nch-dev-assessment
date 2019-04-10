6ba2fa0e6872ec0b2d7a25e23a1f4fee59e35b09
jcabria <julie.cabria@nchcss.com>
2019-04-10 06:29:49 +0100

    Updates

---
+++ precache-manifest.1c9079902c511fb3129271e1956deef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d55f3197d45d09aef29b",
+    "url": "/nch-dev-assessment/static/css/main.00b9a7ea.chunk.css"
+  },
+  {
+    "revision": "d55f3197d45d09aef29b",
+    "url": "/nch-dev-assessment/static/js/main.18e8998d.chunk.js"
+  },
+  {
+    "revision": "37f43ef214f242c35f8c",
+    "url": "/nch-dev-assessment/static/js/runtime~main.79dcdc84.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/nch-dev-assessment/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nch-dev-assessment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "16c3e43f107a82ab903d1af74220515e",
+    "url": "/nch-dev-assessment/index.html"
+  }
+];
